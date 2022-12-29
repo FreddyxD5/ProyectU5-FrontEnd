293a2944e6801a4398b2f978511bd975627757f1
FreddyxD5 <freddyxd5@gmail.com>
2022-12-29 17:59:06 -0500

    alert

--- js/index.js
+++ js/index.js
@@ -44,54 +44,63 @@ async function login() {
         email: email.value,
         password: password.value
     }
-    const response = await fetch(`${URL_API}/login/`, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    const datos = await response.json()
-    loginSucess  = Object.keys(datos).length
-    if (loginSucess>1) {
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 1500,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-              toast.addEventListener('mouseenter', Swal.stopTimer)
-              toast.addEventListener('mouseleave', Swal.resumeTimer)
-            }
-          })
-          
-          Toast.fire({
-            icon: 'success',
-            title: 'Bienvenido de nuevo!',
-            text: `${datos['email']}`
-          })          
-          localStorage.setItem('access', `${datos['tokens']['access']}`)
-          localStorage.setItem('refresh', `${datos['tokens']['refresh']}`)        
-          localStorage.setItem('email', `${datos['email']}`) 
-          if (datos['is_superuser']){
-            setTimeout(()=>{
-                window.location.replace("./pages/adashboard.html");
-              },1500)        
-          }else{
-            setTimeout(()=>{
-                window.location.replace("./pages/udashboard.html");
-            },1500)                  
-          }
-                  
-    }else{
+    try{
+        const response = await fetch(`${URL_API}/login/`, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        const datos = await response.json()
+        loginSucess  = Object.keys(datos).length
+        if (loginSucess>1) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1500,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              Toast.fire({
+                icon: 'success',
+                title: 'Bienvenido de nuevo!',
+                text: `${datos['email']}`
+              })          
+              localStorage.setItem('access', `${datos['tokens']['access']}`)
+              localStorage.setItem('refresh', `${datos['tokens']['refresh']}`)        
+              localStorage.setItem('email', `${datos['email']}`) 
+              if (datos['is_superuser']){
+                setTimeout(()=>{
+                    window.location.replace("./pages/adashboard.html");
+                  },1500)        
+              }else{
+                setTimeout(()=>{
+                    window.location.replace("./pages/udashboard.html");
+                },1500)                  
+              }
+                      
+        }else{
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Parece que algo salio mal!'            
+              })
+        }
+    }catch(err){
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Parece que algo salio mal!'            
+            text: 'Parece que hay un error en el servidor!'            
           })
     }
+    
 }
 
 
